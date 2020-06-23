8353c09feb52c688a28bf921d0beae673b878547
Gaiya M Obed <gaiyaobed94@gmail.com>
2020-06-23 04:16:04 +0100

    bug: fixed bug on axios.js filr

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
 axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
-axios.defaults.headers.common['Content-Type'] = 'Application/json';
+axios.defaults.headers.post['Content-Type'] = 'Application/json';
 ;
 axios.interceptors.request.use(request => {
     console.log(request);
