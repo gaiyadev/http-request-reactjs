a1e48a80af9c9ef366d1e7b3724a976a48d9d2bb
Gaiya M Obed <gaiyaobed94@gmail.com>
2020-06-23 02:39:26 +0100

    Add: getting data from jsonplaceholder site and rendering in the component

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react';
 
 import './FullPost.css';
+import axios from 'axios';
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
+    constructor(props) {
+        super(props)
+        this.state = {
+            loadedPost: null
+        }
+    }
+    componentDidUpdate(prevProps, prevState) {
+        if (this.props.id) {
+            axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id).then(response => {
+                this.setState({ loadedPost: response.data });
+                console.log(response);
 
-        );
+            }).catch(err => console.log(err));
+
+        }
+    }
+    render() {
+        let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
+        if (this.props.id) {
+            post = <p style={{ color: 'red' }} > Loading</p>
+        }
+
+        if (this.state.loadedPost) {
+            post = (
+                <div className="FullPost">
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
+                    <div className="Edit">
+                        <button className="Delete">Delete</button>
+                    </div>
+                </div>
+
+            );
+        }
         return post;
     }
 }
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,18 +4,47 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from 'axios';
+import post from '../../components/Post/Post';
 
 class Blog extends Component {
-    render () {
+    constructor(props) {
+        super(props)
+        this.state = {
+            posts: [],
+            selectedPostId: null
+        }
+    }
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
+            const posts = response.data.slice(4, 8);
+            const updatedPost = posts.map(post => {
+                return {
+                    ...post,
+                    author: 'Obed G'
+                }
+            });
+            this.setState({ posts: updatedPost });
+            //console.log(response);
+        }).catch(err => console.log(err));
+    }
+
+    selectedPostHandler = (id) => {
+        this.setState({ selectedPostId: id })
+    }
+    render() {
+        const posts = this.state.posts.map(post => {
+            return <Post key={post.id} title={post.title} author={post.author} body={post.body}
+                clicked={() => this.selectedPostHandler(post.id)} />
+        });
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
+
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
