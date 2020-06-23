53672c076b74dcef1a38dae53c2476c31ec06ec6
Gaiya M Obed <gaiyaobed94@gmail.com>
2020-06-23 03:18:23 +0100

    add: deleting post

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -23,6 +23,12 @@ class FullPost extends Component {
         }
 
     }
+
+    deletePostHandler = () => {
+        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id).then(res => {
+            console.log(res);
+        }) .catch(err => console.log(err));
+    }
     render() {
         let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
         if (this.props.id) {
@@ -35,7 +41,7 @@ class FullPost extends Component {
                     <h1>{this.state.loadedPost.title}</h1>
                     <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button onClick={this.deletePostHandler} className="Delete">Delete</button>
                     </div>
                 </div>
 
